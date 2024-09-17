cf5b5ac59231b1ee8970789c99f2f35e8ddab025
Joao Almirante <joao.almirante@EMC.LOCAL>
2024-09-17 07:35:07 -0300

    Funcao_Depositar

--- banking_system02.py
+++ banking_system02.py
@@ -12,3 +12,11 @@ def menu():
     [q]\tSair
     ==> """
 
+def depositar(saldo, valor, extrato, /):
+    if valor > 0:
+        saldo += valor
+        extrato += f"## Depósito realizado com sucesso! ##"
+    else:
+        print("\n@@@ Operação não realizada!! Valor informado é inválido")
+    return saldo, extrato
+
