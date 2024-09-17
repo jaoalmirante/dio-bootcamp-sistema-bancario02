64ca4c58d65f2e945eb6759ec1cb1e89093bf39d
Joao Almirante <joao.almirante@EMC.LOCAL>
2024-09-17 08:42:56 -0300

    Funcoes_Usuario

--- banking_system02.py
+++ banking_system02.py
@@ -47,3 +47,22 @@ def exibir_extrato(saldo, /, *, extrato):
     print("Não foram realizadas movimentações." if not extrato else extrato)
     print(f"\nSaldo: \t \t R$ {saldo:.2f}")
     print("####################################")
+
+def criar_usuario(usuarios):
+    cpf = input("Informe o CPF(Apenas Números): ")
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print(f"\nJá exites um usuário com esse CPF: ({cpf}) ")
+        return
+
+    nome = input("Informe o seu nome completo: ")
+    data_nascimento = input("Informe a data de nascimento (dd-mm-aaaa): ")
+    endereco = input("Informe o endereço (logradouro, nº - bairro - cidade/sigla estado): ")
+
+    usuarios.append({'nome': nome, 'data_nascimento': data_nascimento, 'cpf': cpf, 'endereco': endereco})
+
+def filtrar_usuario(cpf, usuarios):
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
+
