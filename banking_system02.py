99b70a670a73e59eaee967dcc2fcf2f8dee3bafe
Joao Almirante <joao.almirante@EMC.LOCAL>
2024-09-17 10:38:58 -0300

    Funcao_Main

--- banking_system02.py
+++ banking_system02.py
@@ -11,6 +11,7 @@ def menu():
     [nu]\tNovo Usuário
     [q]\tSair
     ==> """
+    return input(menu)
 
 def depositar(saldo, valor, extrato, /):
     if valor > 0:
@@ -83,3 +84,60 @@ def listar_contas(contas):
             Titular:\t{conta['usuario']['nome']}
             '''
         print("="*100)
+        print(linha)
+def main():
+    LIMITE_SAQUES = 3
+    AGENCIA = "0001"
+
+    saldo = 0
+    limite = 500
+    extrato = ""
+    numero_saques = 0
+    usuarios = []
+    contas = []
+
+
+    while True:
+        opcao = menu()
+
+        if opcao == "d":
+            valor = float(input("Informe o valor do depósito: "))
+
+            saldo, extrato = depositar(saldo, valor, extrato)
+
+        elif opcao == "s":
+            valor = float(input("Informe o valor do saque: "))
+
+            saldo, extrato = sacar(
+                saldo=saldo,
+                valor=valor,
+                extrato=extrato,
+                limite=limite,
+                numero_saques=numero_saques,
+                limite_saques=LIMITE_SAQUES,
+            )
+
+        elif opcao == "e":
+            exibir_extrato(saldo, extrato=extrato)
+
+        elif opcao == "nu":
+            criar_usuario(usuarios)
+
+        elif opcao == "nc":
+            numero_conta = len(contas) + 1
+            conta = criar_conta(AGENCIA, numero_conta, usuarios)
+
+            if conta:
+                contas.append(conta)
+
+        elif opcao == "lc":
+            listar_contas(contas)
+
+        elif opcao == "q":
+            break
+
+        else:
+            print("Operação inválida, por favor selecione novamente a operação desejada.")
+
+
+main()
