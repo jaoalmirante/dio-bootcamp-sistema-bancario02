8235633de2c6042526ff8a490698dca53c2418c0
Joao Almirante <joao.almirante@EMC.LOCAL>
2024-09-17 07:43:48 -0300

    Funcao_Sacar

--- banking_system02.py
+++ banking_system02.py
@@ -17,6 +17,28 @@ def depositar(saldo, valor, extrato, /):
         saldo += valor
         extrato += f"## Depósito realizado com sucesso! ##"
     else:
-        print("\n@@@ Operação não realizada!! Valor informado é inválido")
+        print("\nOperação não realizada!! Valor informado é inválido")
+    return saldo, extrato
+
+def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
+    excedeu_saldo = valor > saldo
+    excedeu_limite = valor > limite
+    excedeu_saques = numero_saques >= limite_saques
+
+    if excedeu_saldo:
+        print("\n Operção falhou!! Você não saldo suficiente para essa operação!")
+
+    elif excedeu_limite:
+        print("\n Operação falhou!! O valor do saque excede o limite!")
+    elif excedeu_saques:
+        print("\n Operação falhou!! Você excedeu o limite de saques")
+    elif valor > 0:
+        saldo -= valor
+        extrato += f"Saque: \t \tR$ {valor:.2f}\n"
+        numero_saques += 1
+        print("\n O saque foi realizado com sucesso!!")
+    else:
+        print("\nOperação falhou!! O Valor informado é inválido!")
+
     return saldo, extrato
 
