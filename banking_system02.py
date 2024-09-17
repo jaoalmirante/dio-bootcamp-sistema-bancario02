60c0d2c2e2a546e6e5847e97fcd25cf04c5cf753
Joao Almirante <joao.almirante@EMC.LOCAL>
2024-09-17 07:49:36 -0300

    Funcao_Exibir_Extrato

--- banking_system02.py
+++ banking_system02.py
@@ -42,3 +42,8 @@ def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
 
     return saldo, extrato
 
+def exibir_extrato(saldo, /, *, extrato):
+    print("\n############# EXTRATO ############")
+    print("Não foram realizadas movimentações." if not extrato else extrato)
+    print(f"\nSaldo: \t \t R$ {saldo:.2f}")
+    print("####################################")
